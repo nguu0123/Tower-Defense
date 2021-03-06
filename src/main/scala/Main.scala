f50146acc20551d90d487b95806a44ed13d926c1
nguu0 <dung.nguyenanh@aalto.fi>
2021-03-06 15:51:40 +0700

    Fixed the not full scene by letting it auto choose size

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,12 +7,10 @@ import scalafx.scene.paint.Color
 object test extends JFXApp {
 stage = new JFXApp.PrimaryStage {
     title.value = "Test"
-    width = 1920
-    height = 1080
 }
       val map = Array(
     Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-    Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+    Array(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
     Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
@@ -45,8 +43,7 @@ stage = new JFXApp.PrimaryStage {
     Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     )
 val grid = new Grid(1920, 1080)
-grid.info()
-
+grid.loadMap(map)
 val root = new Pane //Simple pane component
 val scene = new Scene(root) //Scene acts as a container for the scene graph
 val canvas = new Canvas(1920, 1080) //Give width and height as a parameter.
